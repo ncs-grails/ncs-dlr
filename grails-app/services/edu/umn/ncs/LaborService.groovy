240907b6eef1888c1e9f4d16bcffca4ecc4a91bc
Simone Q. Vuong <sqv@umn.edu>
2011-04-22 21:25:48 +0000

    revised current functions and create new ones
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-dlr@243 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/services/edu/umn/ncs/LaborService.groovy
+++ grails-app/services/edu/umn/ncs/LaborService.groovy
@@ -8,24 +8,22 @@ class LaborService {
 
     def getReportingStaff(principal) {
 
-        def username = principal.getUsername()  // getUsername() came from UMN Acegi
-        def reportingStaff = ReportingStaff.findByUsername(username)
+        def uname = principal.getUsername()          //getUsername() came from UMN Acegi
+        // println "PRINTLN LaborService.getReportingStaff.principal.getUsername().uname: ${uname}"
 
+        def reportingStaff = ReportingStaff.findByUsername(uname)
+        // println "PRINTLN LaborService.getReportingStaff.ReportingStaff.findByUsername(uname).reportingStaff: ${reportingStaff}"
+        
         if ( !reportingStaff ) {
 
-            // see edu.umn.auth.UmnCookiesUserDetails
-            reportingStaff = new ReportingStaff(username:username)
+            // get info about logged user from cookie (see edu.umn.auth.UmnCookiesUserDetails)
+            reportingStaff = new ReportingStaff(username:uname)
             reportingStaff.email = principal.getEmail()
             reportingStaff.fullName = principal.getFullName()
-            reportingStaff.userCreated = username
-            if ( debug ) {
-                println "PRINTLN userService.getReportingStaff.getInfo: ${principal.getInfo()}"
-            }
-
+            reportingStaff.userCreated = uname
+            // println "PRINTLN LaborService.getReportingStaff.if(!reportingStaff).getInfo: ${principal.getInfo()}"
+        
             def nameParts = reportingStaff.fullName.tokenize()
-            if ( debug ) {
-                println "PRINTLN userService.getReportingStaff.nameParts ${nameParts.size()}"
-            }
             if ( nameParts.size() == 2 ) {
                 reportingStaff.firstName = nameParts[0]
                 reportingStaff.lastName = nameParts[1]
@@ -35,39 +33,75 @@ class LaborService {
                 reportingStaff.middleInit = nameParts[1]
                 reportingStaff.lastName = nameParts[2]
             }
-            if ( debug ) {
-                println "PRINTLN userService.getReportingStaff.nameParts[0] ${nameParts[0]}"
-                println "PRINTLN userService.getReportingStaff.nameParts[1] ${nameParts[1]}"
-                println "PRINTLN userService.getReportingStaff.nameParts[2] ${nameParts[2]}"
-            }
+            // println "PRINTLN LaborService.getReportingStaff.nameParts = ${nameParts.size()}"
+            // println "PRINTLN LaborService.getReportingStaff.nameParts[0] = ${nameParts[0]}"
+            // println "PRINTLN LaborService.getReportingStaff.nameParts[1] = ${nameParts[1]}"
+            // println "PRINTLN LaborService.getReportingStaff.nameParts[2] = ${nameParts[2]}"
 
             reportingStaff.save(flush:true)
 
-
         } //if ( !reportingStaff )
 
+        // println "PRINTLN LaborService.getReportingStaff.return.reportingStaff: ${reportingStaff}"
         return reportingStaff
 
     } //def getReportingStaff(principal)
 
     def getCurrentReportingPeriod() {
 
+        // get today's date
         def today = new LocalDate()
-        def midnight = new LocalTime(0, 0)
-        def lastMonth = today.minusMonths(1)
-        lastMonth = lastMonth.minusDays(lastMonth.dayOfMonth - 1)
+        // println "PRINTLN LaborService.getCurrentReportingPeriod.today = ${today}"
 
-        // Now we have the DateTime of midnight of the first day of last month!
+        def midnight = new LocalTime(0, 0)
+        // println "PRINTLN LaborService.getCurrentReportingPeriod.midnight = ${midnight}"
+
+        // get first day of last month based on today's date
+        def lastMonth = today.minusMonths(1)
+        // println "PRINTLN LaborService.getCurrentReportingPeriod.today.minusMonths(1).lastMonth = ${lastMonth}"
+        lastMonth = lastMonth.minusDays(lastMonth.dayOfMonth - 1)
+        // println "PRINTLN LaborService.getCurrentReportingPeriod.lastMonth = ${lastMonth}"
+
         def lastMonthDate = lastMonth.toDateTime(midnight).toCalendar().getTime()
+        // println "PRINTLN LaborService.getCurrentReportingPeriod.lastMonthDate = ${lastMonthDate}"
 
         def reportingPeriodInstance = ReportingPeriod.findByPeriodDate(lastMonthDate)
+        // println "PRINTLN LaborService.getCurrentReportingPeriod.ReportingPeriod.findByPeriodDate(lastMonthDate).reportingPeriodInstance = ${reportingPeriodInstance}"
 
-        if (!reportingPeriodInstance ) {
-            // create a new reportin period
-        } //if (!reportingPeriodInstance )
+        //if current reporting period does not exist in db, insert a new one in there
+        if ( !reportingPeriodInstance ) {
+            reportingPeriodInstance = new ReportingPeriod(periodDate:lastMonthDate)
+            reportingPeriodInstance.save(flush:true)
+            // println "PRINTLN LaborService.getCurrentReportingPeriod.reportingPeriodInstance = ${reportingPeriodInstance}"
+        }
 
         return reportingPeriodInstance
 
     } //def getCurrentReportingPeriod()
+
+
+    def getNextReportingPeriodDateTime(paramReportingPeriodDate) {
+
+        def theReportingPeriodDate = new LocalDate(paramReportingPeriodDate)
+        //println "PRINTLN LaborService.getNextReportingPeriodDate.theReportingPeriodDate = ${theReportingPeriodDate}"
+
+        def midnight = new LocalTime(0, 0)
+
+        def nextReportingPeriodDate = theReportingPeriodDate.plusMonths(1)
+        //println "PRINTLN LaborService.getNextReportingPeriodDate.nextReportingPeriodDate = ${nextReportingPeriodDate}"
+
+        def nextReportingPeriodDateTime = nextReportingPeriodDate.toDateTime(midnight).toCalendar().getTime()
+
+        return nextReportingPeriodDateTime
+
+    } //def getNextReportingPeriodDate
+
+
+    def getNoncommittedReportedEffort() {
+
+        def nonCommittedReportedEffort = AssignedEffort.findAllByDateCommittedIsNull()
+        // println "PRINTLN LaborService.getNoncommittedReportedEffort.nonCommittedReportedEffort = ${nonCommittedReportedEffort}"
+    }
+
     
-}
+} //class LaborService
