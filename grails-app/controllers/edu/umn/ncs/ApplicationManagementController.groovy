59fbb8b0db2438b762004c5f4fc2cd31709b1bc8
Simone Q. Vuong <sqv@umn.edu>
2011-05-02 22:40:05 +0000

    add effortAssignmentList
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-dlr@276 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/controllers/edu/umn/ncs/ApplicationManagementController.groovy
+++ grails-app/controllers/edu/umn/ncs/ApplicationManagementController.groovy
@@ -15,12 +15,14 @@ class ApplicationManagementController {
     }
 
     def assign = {
+        
+        // GET PERIODS FOR PERIOD CONTROL --------------------------------------
 
-        // get CURRENT PERIOD --------------------------------------------------
+        // get current period 
         def currentPeriod = laborService.getCurrentReportingPeriod()
         println "PRINTLN ApplicationManagementController.assign.currentPeriod: ${currentPeriod}"
 
-        // get NEXT PERIOD, after current period -------------------------------
+        // get next period, after current period 
         def c = ReportingPeriod.createCriteria()
         def nextPeriod = c.list{
             and {
@@ -38,7 +40,7 @@ class ApplicationManagementController {
             // println "PRINTLN ApplicationManagementController.assign.nextPeriod: ${nextPeriod}"
         }
 
-       // get PREVIOUS TWO PERIODS, before current period ----------------------
+       // get previous two periods, before current period 
         def c2 = ReportingPeriod.createCriteria()
         def previousTwoPeriods = c2.list{
             and {
@@ -61,25 +63,79 @@ class ApplicationManagementController {
         }
         */
 
-        // get the appropriate REPORTING PERIOD to display in Effort Assignment table
+        // GET DATA FOR ASSIGNED EFFORT TABLE
+        
+        // get the selected REPORTING PERIOD to display in Effort Assignment table
         def reportingPeriodInstance = ReportingPeriod.read(params?.reportingPeriodInstance?.id)
         println "PRINTLN ApplicationManagementController.assign.(params?.reportingPeriod?.id).reportingPeriodInstance: ${reportingPeriodInstance}"
         
-        // if no reportingPeriod param, assume current period
+        // if no reportingPeriod selected, assume current period
         if ( ! reportingPeriodInstance ) {
             reportingPeriodInstance = laborService.getCurrentReportingPeriod()
             println "PRINTLN ApplicationManagementController.assign.laborService.getCurrentReportingPeriod().reportingPeriodInstance: ${reportingPeriodInstance}"
         }
         
-        // get reportingPeriodMonthName
-        //def reportingreportingPeriodMonth = reportingPeriodInstance.getMonth()
-        //println "PRINTLN ApplicationManagementController.assign.reportingreportingPeriodMonth: ${reportingreportingPeriodMonth}"
-        //def reportingreportingPeriodMonthName = laborService.getMonthName(reportingreportingPeriodMonth)
-        //println "PRINTLN ApplicationManagementController.assign.reportingreportingPeriodMonthName: ${reportingreportingPeriodMonthName}"
-
+       
+        def effortAssignmentList = []
+        
+        def reportingStaffInstanceList = ReportingStaff.list([sort:'lastName'])
+        
+        reportingStaffInstanceList.eachWithIndex{ rs, i ->
+            
+            // row number
+            def record = [:]
+            record.rowNum = i + 1
+            
+            // staff full name
+            record.fullName = rs.fullName
+            record.staffId = rs.id
+                        
+            // effort of current period
+            def currentAssignedEffort = AssignedEffort.findByPeriodAndReportingStaff(reportingPeriodInstance, rs)
+            record.currentPeriodEffort = currentAssignedEffort?.assignedEffort
+            record.isCommitted = currentAssignedEffort?.dateCommitted ? true : false
+                    
+            // effort of previous period
+            def previousPeriod = reportingPeriodInstance.previousPeriod
+            def previousAssignedEffort = AssignedEffort.findByPeriodAndReportingStaff(previousPeriod, rs)
+            record.previousPeriodEffort = previousAssignedEffort?.assignedEffort
+            if ( record.previousPeriodEffort ) {
+                record.previousPeriodEffort = record.previousPeriodEffort / 100
+            }
+            //record.previousPeriodEffort = 42/100
+            
+            record.percentCommitted = currentAssignedEffort?.dateCommitted ? AssginedEffort.assignedEffort : null
+            if ( record.percentCommitted ) {
+                record.percentCommitted = record.percentCommitted / 100
+            }
+            //record.percentCommitted = 33/100
+            
+            // Date Committed
+            record.dateCommitted = currentAssignedEffort?.dateCommitted
+            
+            record.datesEmailSent = currentAssignedEffort?.emails?.sort{it.dateSent}?.collect{g.formatDate(date:it.dateSent, format:'MM-dd-yyyy')}
+            
+            
+            
+            
+            
+            
+            effortAssignmentList.add(record)
+            
+        }
+        
+        def periodSelectList = []
+        periodList.each{
+            periodSelectList.add([id:it.id, name: g.formatDate(date:it.periodDate, format:'MMMM yyyy')])
+        }
+        
+        
+        
         [
             periodList: periodList,
-            reportingPeriodInstance: reportingPeriodInstance 
+            reportingPeriodInstance: reportingPeriodInstance, 
+            effortAssignmentList: effortAssignmentList,
+            periodSelectList: periodSelectList
         ]
 
     } //def assign
