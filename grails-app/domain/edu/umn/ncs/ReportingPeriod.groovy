b1a0cdc9dd3eaca9877764d5a86b5a24be02cd55
Simone Q. Vuong <sqv@umn.edu>
2011-05-02 22:44:58 +0000

    add transient previousPeriod and nextPeriod
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-dlr@280 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/domain/edu/umn/ncs/ReportingPeriod.groovy
+++ grails-app/domain/edu/umn/ncs/ReportingPeriod.groovy
@@ -9,7 +9,7 @@ class ReportingPeriod {
     Date completedReportDate
 
     static hasMany = [assignedEfforts: AssignedEffort]
-    static transients = ['year', 'month']
+    static transients = ['year', 'month', 'previousPeriod', 'nextPeriod']
 
     Integer getYear() {
         def localYear = new LocalDate(periodDate)
@@ -18,6 +18,22 @@ class ReportingPeriod {
     Integer getMonth() {
         def localMonth = new LocalDate(periodDate)
         return localMonth.monthOfYear
+    }
+    
+    ReportingPeriod getPreviousPeriod() {
+        ReportingPeriod.createCriteria().get{
+            lt("periodDate", periodDate)
+            maxResults(1)
+            order("periodDate","desc")
+        }
+    }
+
+    ReportingPeriod getNextPeriod() {
+        ReportingPeriod.createCriteria().get{
+            gt("periodDate", periodDate)
+            maxResults(1)
+            order("periodDate")
+        }
     }
 
     String toString() {
