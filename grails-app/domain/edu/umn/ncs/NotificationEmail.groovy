1a7b29c17c631142e968c592bbe43f25fb9d0d68
Simone Q. Vuong <sqv@umn.edu>
2011-05-12 22:43:47 +0000

    add getYear, getMonth, getDay
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-dlr@329 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/domain/edu/umn/ncs/NotificationEmail.groovy
+++ grails-app/domain/edu/umn/ncs/NotificationEmail.groovy
@@ -1,15 +1,30 @@
 package edu.umn.ncs
+import org.joda.time.*
 
 class NotificationEmail {
 
     Date dateSent = new Date()
     String userSent
 
-    String toString() {
-        "Email sent on ${dateSent}"
-    } 
-
     static belongsTo = [assignedEffort: AssignedEffort]
+    static transients = ['year', 'month', 'day']
+
+    Integer getYear() {
+        def localYear = new LocalDate(periodDate)
+        return localYear.year
+    }
+    Integer getMonth() {
+        def localMonth = new LocalDate(periodDate)
+        return localMonth.monthOfYear
+    }
+    Integer getDay() {
+        def localDay = new LocalDate(periodDate)
+        return localDay.dayOfMonth
+    }
+
+    String toString() {
+        "${dateSent}"
+    }
 
     static constraints = {
         assignedEffort()
