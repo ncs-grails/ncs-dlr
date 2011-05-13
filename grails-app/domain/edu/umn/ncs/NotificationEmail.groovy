9e56b4c4ca16817cafddf4832cf58415b68ff958
Simone Q. Vuong <sqv@umn.edu>
2011-05-13 22:49:08 +0000

    move code around
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-dlr@342 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/domain/edu/umn/ncs/NotificationEmail.groovy
+++ grails-app/domain/edu/umn/ncs/NotificationEmail.groovy
@@ -7,21 +7,7 @@ class NotificationEmail {
     String userSent
 
     static belongsTo = [assignedEffort: AssignedEffort]
-    static transients = ['year', 'month', 'day']
-
-    Integer getYear() {
-        def localYear = new LocalDate(periodDate)
-        return localYear.year
-    }
-    Integer getMonth() {
-        def localMonth = new LocalDate(periodDate)
-        return localMonth.monthOfYear
-    }
-    Integer getDay() {
-        def localDay = new LocalDate(periodDate)
-        return localDay.dayOfMonth
-    }
-
+   
     String toString() {
         "${dateSent}"
     }
