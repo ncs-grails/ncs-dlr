1dc542282f409c51b6854bfa9306dc98ee1e2aa7
Simone Q. Vuong <sqv@umn.edu>
2011-05-23 22:28:57 +0000

    add controller for effort user reports
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-dlr@386 59f948a9-288f-427a-98b3-c46ff3be0d89

---
+++ grails-app/controllers/edu/umn/ncs/ReportedEffortController.groovy
@@ -0,0 +1,100 @@
+package edu.umn.ncs
+
+class ReportedEffortController {
+
+    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+
+    def index = {
+        redirect(action: "list", params: params)
+    }
+
+    def list = {
+        params.max = Math.min(params.max ? params.int('max') : 10, 100)
+        [reportedEffortInstanceList: ReportedEffort.list(params), reportedEffortInstanceTotal: ReportedEffort.count()]
+    }
+
+    def create = {
+        def reportedEffortInstance = new ReportedEffort()
+        reportedEffortInstance.properties = params
+        return [reportedEffortInstance: reportedEffortInstance]
+    }
+
+    def save = {
+        def reportedEffortInstance = new ReportedEffort(params)
+        if (reportedEffortInstance.save(flush: true)) {
+            flash.message = "${message(code: 'default.created.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), reportedEffortInstance.id])}"
+            redirect(action: "show", id: reportedEffortInstance.id)
+        }
+        else {
+            render(view: "create", model: [reportedEffortInstance: reportedEffortInstance])
+        }
+    }
+
+    def show = {
+        def reportedEffortInstance = ReportedEffort.get(params.id)
+        if (!reportedEffortInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            [reportedEffortInstance: reportedEffortInstance]
+        }
+    }
+
+    def edit = {
+        def reportedEffortInstance = ReportedEffort.get(params.id)
+        if (!reportedEffortInstance) {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), params.id])}"
+            redirect(action: "list")
+        }
+        else {
+            return [reportedEffortInstance: reportedEffortInstance]
+        }
+    }
+
+    def update = {
+        def reportedEffortInstance = ReportedEffort.get(params.id)
+        if (reportedEffortInstance) {
+            if (params.version) {
+                def version = params.version.toLong()
+                if (reportedEffortInstance.version > version) {
+                    
+                    reportedEffortInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'reportedEffort.label', default: 'ReportedEffort')] as Object[], "Another user has updated this ReportedEffort while you were editing")
+                    render(view: "edit", model: [reportedEffortInstance: reportedEffortInstance])
+                    return
+                }
+            }
+            reportedEffortInstance.properties = params
+            if (!reportedEffortInstance.hasErrors() && reportedEffortInstance.save(flush: true)) {
+                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), reportedEffortInstance.id])}"
+                redirect(action: "show", id: reportedEffortInstance.id)
+            }
+            else {
+                render(view: "edit", model: [reportedEffortInstance: reportedEffortInstance])
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), params.id])}"
+            redirect(action: "list")
+        }
+    }
+
+    def delete = {
+        def reportedEffortInstance = ReportedEffort.get(params.id)
+        if (reportedEffortInstance) {
+            try {
+                reportedEffortInstance.delete(flush: true)
+                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), params.id])}"
+                redirect(action: "list")
+            }
+            catch (org.springframework.dao.DataIntegrityViolationException e) {
+                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), params.id])}"
+                redirect(action: "show", id: params.id)
+            }
+        }
+        else {
+            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'reportedEffort.label', default: 'ReportedEffort'), params.id])}"
+            redirect(action: "list")
+        }
+    }
+}
