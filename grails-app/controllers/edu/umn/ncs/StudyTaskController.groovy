b3f49869627e9d8d6877e4cd36610b6011e52973
Simone Q. Vuong <sqv@umn.edu>
2012-01-06 22:55:50 +0000

    make list of active Tasks available per application admin request.
    git-svn-id: https://svn.cccs.umn.edu/svn/ncs/ncs-dlr@798 59f948a9-288f-427a-98b3-c46ff3be0d89

--- grails-app/controllers/edu/umn/ncs/StudyTaskController.groovy
+++ grails-app/controllers/edu/umn/ncs/StudyTaskController.groovy
@@ -2,101 +2,111 @@ package edu.umn.ncs
 
 class StudyTaskController {
 
-    static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
+	static allowedMethods = [save: "POST", update: "POST", delete: "POST"]
 
 	def debug = grailsApplication.config.console.debugging
 	
-    def index = {
-        redirect(action: "list", params: params)
-    }
+	def index = {
+		redirect(action: "list", params: params)
+	}
 
-    def list = {
-        params.max = Math.min(params.max ? params.int('max') : 10, 100)
-        [studyTaskInstanceList: StudyTask.list(params), studyTaskInstanceTotal: StudyTask.count()]
-    }
+	def list = {
 
-    def create = {
-        def studyTaskInstance = new StudyTask()
-        studyTaskInstance.properties = params
-        return [studyTaskInstance: studyTaskInstance]
-    }
+		if (debug) {
+			println "STUDY TASK CONTROLLER > INDEX ---------------------"
+			println "=> params: ${params}"
+		}
 
-    def save = {
-        def studyTaskInstance = new StudyTask(params)
-        if (studyTaskInstance.save(flush: true)) {
-            flash.message = "${message(code: 'default.created.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), studyTaskInstance.id])}"
-            redirect(action: "show", id: studyTaskInstance.id)
-        }
-        else {
-            render(view: "create", model: [studyTaskInstance: studyTaskInstance])
-        }
-    }
+		params.max = Math.min(params.max ? params.int('max') : 10, 100)
 
-    def show = {
-        def studyTaskInstance = StudyTask.get(params.id)
-        if (!studyTaskInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            [studyTaskInstance: studyTaskInstance]
-        }
-    }
+		[
+			studyTaskInstanceList: StudyTask.list(params), 
+			//studyTaskInstanceList: StudyTask.list(params), 
+			studyTaskInstanceTotal: StudyTask.count()
 
-    def edit = {
-        def studyTaskInstance = StudyTask.get(params.id)
-        if (!studyTaskInstance) {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
-            redirect(action: "list")
-        }
-        else {
-            return [studyTaskInstance: studyTaskInstance]
-        }
-    }
+		]
 
-    def update = {
-        def studyTaskInstance = StudyTask.get(params.id)
-        if (studyTaskInstance) {
-            if (params.version) {
-                def version = params.version.toLong()
-                if (studyTaskInstance.version > version) {
-                    
-                    studyTaskInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'studyTask.label', default: 'StudyTask')] as Object[], "Another user has updated this StudyTask while you were editing")
-                    render(view: "edit", model: [studyTaskInstance: studyTaskInstance])
-                    return
-                }
-            }
-            studyTaskInstance.properties = params
-            if (!studyTaskInstance.hasErrors() && studyTaskInstance.save(flush: true)) {
-                flash.message = "${message(code: 'default.updated.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), studyTaskInstance.id])}"
-                redirect(action: "show", id: studyTaskInstance.id)
-            }
-            else {
-                render(view: "edit", model: [studyTaskInstance: studyTaskInstance])
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
-            redirect(action: "list")
-        }
-    }
+	}
+/*
+	def create = {
+		def studyTaskInstance = new StudyTask()
+		studyTaskInstance.properties = params
+		return [studyTaskInstance: studyTaskInstance]
+	}
 
-    def delete = {
-        def studyTaskInstance = StudyTask.get(params.id)
-        if (studyTaskInstance) {
-            try {
-                studyTaskInstance.delete(flush: true)
-                flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
-                redirect(action: "list")
-            }
-            catch (org.springframework.dao.DataIntegrityViolationException e) {
-                flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
-                redirect(action: "show", id: params.id)
-            }
-        }
-        else {
-            flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
-            redirect(action: "list")
-        }
-    }
+	def save = {
+		def studyTaskInstance = new StudyTask(params)
+		if (studyTaskInstance.save(flush: true)) {
+		    flash.message = "${message(code: 'default.created.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), studyTaskInstance.id])}"
+		    redirect(action: "show", id: studyTaskInstance.id)
+		} else {
+		    render(view: "create", model: [studyTaskInstance: studyTaskInstance])
+		}
+	}
+
+	def show = {
+		def studyTaskInstance = StudyTask.get(params.id)
+		if (!studyTaskInstance) {
+		    flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
+		    redirect(action: "list")
+		} else {
+		    [studyTaskInstance: studyTaskInstance]
+		}
+	}
+
+	def edit = {
+		def studyTaskInstance = StudyTask.get(params.id)
+		if (!studyTaskInstance) {
+		    flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
+		    redirect(action: "list")
+		} else {
+		    return [studyTaskInstance: studyTaskInstance]
+		}
+	}
+
+	def update = {
+		def studyTaskInstance = StudyTask.get(params.id)
+		if (studyTaskInstance) {
+		    if (params.version) {
+			def version = params.version.toLong()
+			if (studyTaskInstance.version > version) {
+		    
+			    studyTaskInstance.errors.rejectValue("version", "default.optimistic.locking.failure", [message(code: 'studyTask.label', default: 'StudyTask')] as Object[], "Another user has updated this StudyTask while you were editing")
+			    render(view: "edit", model: [studyTaskInstance: studyTaskInstance])
+			    return
+			}
+		    }
+		    studyTaskInstance.properties = params
+		    if (!studyTaskInstance.hasErrors() && studyTaskInstance.save(flush: true)) {
+			flash.message = "${message(code: 'default.updated.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), studyTaskInstance.id])}"
+			redirect(action: "show", id: studyTaskInstance.id)
+		    } else {
+			render(view: "edit", model: [studyTaskInstance: studyTaskInstance])
+		    }
+		}
+		else {
+		    flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
+		    redirect(action: "list")
+		}
+	}
+
+	def delete = {
+		def studyTaskInstance = StudyTask.get(params.id)
+		if (studyTaskInstance) {
+		    try {
+			studyTaskInstance.delete(flush: true)
+			flash.message = "${message(code: 'default.deleted.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
+			redirect(action: "list")
+		    }
+		    catch (org.springframework.dao.DataIntegrityViolationException e) {
+			flash.message = "${message(code: 'default.not.deleted.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
+			redirect(action: "show", id: params.id)
+		    }
+		} else {
+		    flash.message = "${message(code: 'default.not.found.message', args: [message(code: 'studyTask.label', default: 'StudyTask'), params.id])}"
+		    redirect(action: "list")
+		}
+	}
+*/
+
 }
--- grails-app/views/studyTask/list.gsp
+++ grails-app/views/studyTask/list.gsp
@@ -1,65 +1,48 @@
-
 <%@ page import="edu.umn.ncs.StudyTask" %>
 <html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-        <meta name="layout" content="ncs" />
-        <g:set var="entityName" value="${message(code: 'studyTask.label', default: 'StudyTask')}" />
-        <title><g:message code="default.list.label" args="[entityName]" /></title>
-    </head>
-    <body>
-        <div class="nav">
-            <span class="menuButton"><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></span>
-            <span class="menuButton"><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></span>
-        </div>
-        <div class="body">
-            <h1><g:message code="default.list.label" args="[entityName]" /></h1>
-            <g:if test="${flash.message}">
-            <div class="message">${flash.message}</div>
-            </g:if>
-            <div class="list">
-                <table>
-                    <thead>
-                        <tr>
-                        
-                            <g:sortableColumn property="id" title="${message(code: 'studyTask.id.label', default: 'Id')}" />
-                        
-                            <g:sortableColumn property="name" title="${message(code: 'studyTask.name.label', default: 'Name')}" />
-                        
-                            <g:sortableColumn property="obsolete" title="${message(code: 'studyTask.obsolete.label', default: 'Obsolete')}" />
-                        
-                            <g:sortableColumn property="dateCreated" title="${message(code: 'studyTask.dateCreated.label', default: 'Date Created')}" />
-                        
-                            <g:sortableColumn property="userCreated" title="${message(code: 'studyTask.userCreated.label', default: 'User Created')}" />
-                        
-                            <g:sortableColumn property="appCreated" title="${message(code: 'studyTask.appCreated.label', default: 'App Created')}" />
-                        
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <g:each in="${studyTaskInstanceList}" status="i" var="studyTaskInstance">
-                        <tr class="${(i % 2) == 0 ? 'odd' : 'even'}">
-                        
-                            <td><g:link action="show" id="${studyTaskInstance.id}">${fieldValue(bean: studyTaskInstance, field: "id")}</g:link></td>
-                        
-                            <td>${fieldValue(bean: studyTaskInstance, field: "name")}</td>
-                        
-                            <td><g:formatBoolean boolean="${studyTaskInstance.obsolete}" /></td>
-                        
-                            <td><g:formatDate date="${studyTaskInstance.dateCreated}" /></td>
-                        
-                            <td>${fieldValue(bean: studyTaskInstance, field: "userCreated")}</td>
-                        
-                            <td>${fieldValue(bean: studyTaskInstance, field: "appCreated")}</td>
-                        
-                        </tr>
-                    </g:each>
-                    </tbody>
-                </table>
-            </div>
-            <div class="paginateButtons">
-                <g:paginate total="${studyTaskInstanceTotal}" />
-            </div>
-        </div>
-    </body>
-</html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="layout" content="ncs" />
+		<g:set var="entityName" value="${message(code: 'studyTask.label', default: 'StudyTask')}" />
+		<title><g:message code="default.list.label" args="[entityName]" /></title>
+	</head>
+	<body>
+		<div class="nav">
+			<span class="menuButton"><a class="home" href="${createLink(uri: '/')}"><g:message code="default.home.label"/></a></span>
+			<span class="menuButton"><g:link class="create" action="create"><g:message code="default.new.label" args="[entityName]" /></g:link></span>
+		</div>
+		<div class="body">
+			<h1><g:message code="default.list.label" args="[entityName]" /></h1>
+			<g:if test="${flash.message}">
+				<div class="message">${flash.message}</div>
+			</g:if>
+			<div class="list">
+				<table>
+					<thead>
+						<tr>
+							<g:sortableColumn property="id" title="${message(code: 'studyTask.id.label', default: 'Id')}" />
+							<g:sortableColumn property="name" title="${message(code: 'studyTask.name.label', default: 'Name')}" />
+							<g:sortableColumn property="dateCreated" title="${message(code: 'studyTask.dateCreated.label', default: 'Date Created')}" />
+							<g:sortableColumn property="userCreated" title="${message(code: 'studyTask.userCreated.label', default: 'User Created')}" />
+							<g:sortableColumn property="appCreated" title="${message(code: 'studyTask.appCreated.label', default: 'App Created')}" />
+						</tr>
+					</thead>
+					<tbody>
+						<g:each in="${studyTaskInstanceList}" status="i" var="studyTaskInstance">
+							<tr class="${(i % 2) == 0 ? 'odd' : 'even'}">
+								<td><g:link action="show" id="${studyTaskInstance.id}">${fieldValue(bean: studyTaskInstance, field: "id")}</g:link></td>
+								<td>${fieldValue(bean: studyTaskInstance, field: "name")}</td>
+								<td><g:formatDate date="${studyTaskInstance.dateCreated}" /></td>
+								<td>${fieldValue(bean: studyTaskInstance, field: "userCreated")}</td>
+								<td>${fieldValue(bean: studyTaskInstance, field: "appCreated")}</td>
+							</tr>
+						</g:each>
+					</tbody>
+				</table>
+			</div>
+			<div class="paginateButtons">
+				<g:paginate total="${studyTaskInstanceTotal}" />
+			</div>
+		</div>
+	</body>
+<html>
